d9d289caa88fc61ad08a523fd5ac09dced28b481
unknown <brianna.albo@gmail.com>
2023-01-09 13:39:11 -0500

    trying to fix path issues, added folder for middleware

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const noteRoute = require('./public/notes');
+const noteRoute = require('/notes');
 const app = express();
 
 app.use('/notes', noteRoute);
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-const express = require('express');
+const express = require("express");
+
+
 const path = require('path');
-const routeOne = require('./db/db.json');
-const PORT = 3000;
+const { clog } = require('./middleware/clog')
+const api = require('./routes/index.js')
+const data = require('./db/db.json')
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use('/api', api);
+
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-res.send("Hi")
+res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get(routeOne, (req, res) => {
     res.send("cool")
