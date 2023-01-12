854e42b52087fc4eb6defd88ce6dcb20522b4d67
unknown <brianna.albo@gmail.com>
2023-01-12 13:24:52 -0500

    changed fs utils file, and api routes file. finally was able to have user notes written to db with post req

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,49 +1,34 @@
-const fs = require('fs');
-const util = require('util');
-const uuidv1 = require('uuidv1');
+const fs = require("fs");
+const util = require("util");
 
+// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-function read() {
-  readFromFile('db/db.json', 'utf-8', (err, data) => {
-    res.json(JSON.parse(data))
-})
-  return readFromFile('db/db.json', 'utf8');
-};
-
-function write(note) {
-  return writeFileAsync('db/db.json', JSON.stringify(note));
-};
-
-function getNotes(note) {
-  return this.read().then((note) => {
-    let parsedNotes;
-
-    try {
-      parsedNotes = [].concat(JSON.parse(note));
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
     }
-      catch (err) {
-        parsedNotes = [];
-      }
-    
-
-  return parsedNotes; 
-    })
-  }
-
-
-function newNote(note){
-  const { title, text } = note; 
-
-  const nextNote = { title, text, id: uuidv1() };
-
-  return getNotes(note)
-  .then((notes) => [...notes, nextNote])
-  .then((updatedNotes) => this.write(updatedNotes))
-  .then(() => nextNote);
+  });
 };
 
-
-
-module.exports = { read, write, getNotes, newNote }
+module.exports = { readFromFile, writeToFile, readAndAppend };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,12 @@
 const ROUTER = require('express').Router();
-const { read, write, getNotes, newNote} =  require('../helpers/fsUtils');
+const uuidv1 = require('uuidv1');
+const { readFromFile, writeToFile, readAndAppend } =  require('../helpers/fsUtils');
 
 
 
 ROUTER.get('/notes', (req, res) => {
-    
-    getNotes()
-    .then((notes) => {
-       return res.json(notes)
-    })
-    .catch((err) => res.status(500).json(err))
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+
 });
 
 
@@ -17,14 +14,41 @@ ROUTER.get('/notes', (req, res) => {
 
 
 
+
+ROUTER.get('/notes', (req, res) => {
+    notes.get("/:note_id", (req, res) => {
+        const noteID = req.params.note_id;
+        readFromFile("./db/db.json")
+          .then((data) => JSON.parse(data))
+          .then((json) => {
+            const result = json.filter((note) => note.note_id === noteID);
+            return result.length > 0
+              ? res.json(result)
+              : res.json("invalid note id.");
+          });
+      });
+    
+});
 
 ROUTER.post('/notes', (req, res) => {
+    console.log(req.body);
 
-    newNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-    
-
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv1(),
+      };
+  
+      readAndAppend(newNote, "./db/db.json");
+      res.json(`Note added successfully 🚀`);
+    } else {
+      res.error("Error in adding note");
+    }
 });
+
+
 
 module.exports = ROUTER;
